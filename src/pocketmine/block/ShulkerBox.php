056995f79e1ea7faa51b51539c3bf616783d634f
wesleyvanneck <wesleyvanneck1012@gmail.com>
2017-07-06 14:28:39 +0200

    shulker box still dont open

--- src/pocketmine/block/ShulkerBox.php
+++ src/pocketmine/block/ShulkerBox.php
@@ -21,25 +21,42 @@
 
 namespace pocketmine\block;
 
-use pocketmine\item\Tool;
 use pocketmine\Player;
+use pocketmine\inventory\ShulkerBoxInventory;
+//use pocketmine\inventory\EnderChestInventory;
+use pocketmine\item\enchantment\Enchantment;
+use pocketmine\item\Item;
+use pocketmine\item\Tool;
+use pocketmine\math\AxisAlignedBB;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\nbt\tag\IntTag;
+use pocketmine\nbt\tag\StringTag;
+use pocketmine\tile\ShulkerBox as tiletest;
+use pocketmine\tile\Tile;
 
 class ShulkerBox extends Solid{
 	
 	protected $id = self::SHULKER_BOX;
 	
-	public function __construct($meta = 0){
-		$this->meta = $meta;
-		
-	}
+    public function __construct($meta = 0){
+        $this->meta = $meta;
+    }
 	
-	public function getHardness(){
-		return 6;
-	}
+    public function canBeActivated(){
+        return true;
+    }
 	
-	public function getToolType(){
-		return Tool::TYPE_PICKAXE;
-	}
+    public function getHardness(){
+        return 22.5;
+    }
+	
+    public function getResistance(){
+        return 3000;
+    }
+	
+    public function getToolType(){
+        return Tool::TYPE_PICKAXE;
+    }
 	
 	public function getName(){
 		static $names = [
@@ -62,6 +79,60 @@ class ShulkerBox extends Solid{
 		];
 		return $names[$this->meta & 0x0f];
 		
-		   $player->addWindow($chest->getInventory());
-	}
+        $this->getLevel()->setBlock($block, $this, true, true);
+        $nbt = new CompoundTag("", [
+            new StringTag("id", Tile::SHULKER_BOX),
+            new IntTag("x", $this->x),
+            new IntTag("y", $this->y),
+            new IntTag("z", $this->z)
+        ]);
+		
+        if($item->hasCustomName()){
+            $nbt->CustomName = new StringTag("CustomName", $item->getCustomName());
+        }
+		
+        Tile::createTile(Tile::SHULKER_BOX, $this->getLevel(), $nbt);
+        return true;
+		
+    }
+    public function onActivate(Item $item, Player $player = null){
+        if($player instanceof Player){
+            $top = $this->getSide(1);
+            if($top->isTransparent() !== true){
+                return true;
+            }
+			
+            $t = $this->getLevel()->getTile($this);
+            $chest = null;
+            if($t instanceof tiletest){
+                $chest = $t;
+            }else{
+                $nbt = new CompoundTag("", [
+                    new StringTag("id", Tile::SHULKER_BOX),
+                    new IntTag("x", $this->x),
+                    new IntTag("y", $this->y),
+                    new IntTag("z", $this->z)
+                ]);
+                $chest = Tile::createTile(Tile::SHULKER_BOX, $this->getLevel(), $nbt);
+            }
+			
+            if($chest instanceof tiletest){
+                $player->addWindow(new ShulkerBoxInventory($this, $player));
+				
+            }
+        }
+        return true;
+    }
+	
+    public function getDrops(Item $item){
+        if ($item->hasEnchantments() && $item->getEnchantment(Enchantment::TYPE_MINING_SILK_TOUCH) !== null){
+            return [
+                [$this->id, 0, 1],
+            ];
+        }
+		
+        return [
+            [Item::dirt, 0, 8], //dubug
+        ];
+    }
 }
